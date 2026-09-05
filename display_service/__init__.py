d277e899500236dd417746e22db077878f53a0b8
Karo <76846752+InvalidKaro@users.noreply.github.com>
2026-09-05 22:17:09 +0200

    Enable retro OLED animation cycle

--- display_service/__init__.py
+++ display_service/__init__.py
@@ -1,5 +1,7 @@
 """Physical OLED display service for HomePi."""
 
 from display_service.theme_modern import install as _install_modern_theme
+from display_service.animation_retro import install as _install_retro_animation
 
 _install_modern_theme()
+_install_retro_animation()
